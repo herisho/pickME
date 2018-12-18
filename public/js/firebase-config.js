4a179462d6681045d50ce09fcf0b5e7b3c6e0a05
Kevin <kevin@brounie.com>
2018-12-18 01:21:32 -0600

    [kevin] Added firebase basic config

---
+++ public/js/firebase-config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyACT9N3NFxO2pKdloRP_nEK7sajPPLIMmU",
+  authDomain: "pickme-brounie-eval.firebaseapp.com",
+  databaseURL: "https://pickme-brounie-eval.firebaseio.com",
+  projectId: "pickme-brounie-eval",
+  storageBucket: "pickme-brounie-eval.appspot.com",
+  messagingSenderId: "270154486543"
+};
+firebase.initializeApp(config);
