4d2410f467c93f74b0bb72991c3e5cece1cf62f6
Kevin <kevin@brounie.com>
2018-12-18 21:46:08 -0600

    [kevin] Using firebase-admin to authenticate session

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,53 @@
-var firebase = require('firebase');
-  require('firebase/auth');
-  require('firebase/database');
+// Firebase as Middleware
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
 
-// Initialize Firebase for the application
-var config = {
-  apiKey: "AIzaSyACT9N3NFxO2pKdloRP_nEK7sajPPLIMmU",
-  authDomain: "pickme-brounie-eval.firebaseapp.com",
-  databaseURL: "https://pickme-brounie-eval.firebaseio.com",
-  projectId: "pickme-brounie-eval",
-  storageBucket: "pickme-brounie-eval.appspot.com",
-  messagingSenderId: "270154486543"
-};
-firebase.initializeApp(config); 
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://pickme-brounie-eval.firebaseio.com"
+});
 
 // ================================= isLogged
 function isLogged(req, res, next) {
-  var user = firebase.auth().currentUser;
-  if (user !== null) {
-    req.user = user;
-    next();
+  const authorization = req.header('Authorization');
+  if (authorization) {
+      admin.auth().verifyIdToken(authorization)
+      .then((decodedToken) => {
+          console.log(decodedToken); //Check decoding
+          res.locals.user = decodedToken;
+          next();
+      })
+      .catch(err => {
+          console.log(err);
+          // res.sendStatus(401);
+          res.redirect('/');
+      });
   } else {
-    res.redirect('/');
+      console.log('Authorization header is not found');
+      // res.sendStatus(401);
+      res.redirect('/');
   }
 }
+
+// ================================= isNotLogged
 function isNotLogged(req, res, next) {
-  var user = firebase.auth().currentUser;
-  if (user !== null) {
-    req.user = user;
-    res.redirect('/home');
+  const authorization = req.header('Authorization');
+  if (authorization) {
+      admin.auth().verifyIdToken(authorization)
+      .then((decodedToken) => {
+          console.log(decodedToken); //Check decoding
+          res.locals.user = decodedToken;
+          res.redirect('/home');
+      })
+      .catch(err => {
+          console.log(err);
+          // res.sendStatus(401);
+          next();
+      });
   } else {
-    next();
+      console.log('Authorization header is not found');
+      // res.sendStatus(401);
+      next();
   }
 }
 
